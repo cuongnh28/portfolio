41573fa9195ea47e40f213afaa2ac7aab9ca964c
Hoang-H-Nguyen <hoanghuynguyen247@gmail.com>
2024-11-08 14:03:09 +0700

    adsasd

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
@@ -16,14 +18,6 @@ const nextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    EMAILJS_SERVICE_ID: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
-    EMAILJS_TEMPALTE_ID: process.env.NEXT_PUBLIC_EMAILJS_TEMPALTE_ID,
-    EMAILJS_PUBLIC_KEY: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
-    NOTION_TOKEN: process.env.NEXT_PUBLIC_NOTION_TOKEN,
-    NOTION_DATABASE_ID: process.env.NEXT_PUBLIC_NOTION_DATABASE_ID,
-    HASNODE_API_KEY: process.env.NEXT_PUBLIC_HASHNODE_KEY,
-  },
 };
 
 module.exports = withMDX(nextConfig);
