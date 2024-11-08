1b2ccb1279a66b9ccc596c7f2da9bcf978c645d7
Hoang-H-Nguyen <hoanghuynguyen247@gmail.com>
2024-11-08 14:27:53 +0700

    alex

--- next.config.js
+++ next.config.js
@@ -16,14 +16,6 @@ const nextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    EMAILJS_SERVICE_ID: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
-    EMAILJS_TEMPALTE_ID: process.env.NEXT_PUBLIC_EMAILJS_TEMPALTE_ID,
-    EMAILJS_PUBLIC_KEY: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
-    NOTION_TOKEN: process.env.NEXT_PUBLIC_NOTION_TOKEN,
-    NOTION_DATABASE_ID: process.env.NEXT_PUBLIC_NOTION_DATABASE_ID,
-    HASNODE_API_KEY: process.env.NEXT_PUBLIC_HASHNODE_KEY,
-  },
 };
 
 module.exports = withMDX(nextConfig);
